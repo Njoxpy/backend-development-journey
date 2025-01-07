5aaa06fbdbd189eaab248d2e7b5876f7aa78b16b
Godbless Nyagawa <godblessnyagawa@gmail.com>
2025-01-07 17:50:55 +0300

    added response status code and api response

---
+++ projects/beginner/taskTracker/server.js
@@ -0,0 +1,35 @@
+// Import necessary modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+// Load environment variables
+dotenv.config();
+
+// Initialize the app
+const app = express();
+
+// Middleware
+app.use(cors()); // Allow cross-origin requests
+app.use(bodyParser.json()); // Parse JSON bodies
+
+// routes
+const taskRoutes = require("./routes/task.routes");
+
+// register
+app.use("/api/v1", taskRoutes);
+
+// MongoDB connection setup
+const dbURI = process.env.DB_URI || 'mongodb://localhost:27017/tasktracker'; 
+mongoose.connect(dbURI, {
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Failed to connect to MongoDB', err));
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on localhost:${PORT}`);
+});
