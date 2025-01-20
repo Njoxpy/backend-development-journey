21fd293b9d4bf2d2da5ae3a9f3c8fc7ad4393897
njox <godblessnyagawa@gmail.com>
2025-01-19 19:29:41 -0800

    fix formatting issues in server.js and update package-lock.json

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-const express = require("express")
+const express = require("express");
 require("dotenv").config();
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 const connectDb = require("./config/DB");
 
 app.get("/api/v1/", (req, res) => {
-    res.send({message:"backend development journey"})
-})
+  res.send({ message: "backend development journ  ey" });
+});
 
 app.listen(PORT, () => {
-    connectDb()
-    console.log(`Listening at http://localhost:${PORT}`);
-})
+  connectDb();
+  console.log(`Listening at http://localhost:${PORT}`);
+});
